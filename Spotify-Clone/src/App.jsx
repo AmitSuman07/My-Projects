6a845877c12109987832bac78ef32f41a65b5dee
Amit Suman <136958341+AmitSuman07@users.noreply.github.com>
2024-05-13 15:52:32 +0530

    Add files via upload

---
+++ Spotify-Clone/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import SideBar from './Components/SideBar'
+import Player from './Components/Player'
+import Display from './Components/Display'
+import { PlayerContext } from './Context/PlayerContext'
+
+function App() {
+
+  const {audioRef,track}= useContext(PlayerContext);
+  return (
+   <div className='h-screen bg-black'>
+    <div className='h-[90%] flex '>
+      <SideBar/>
+      <Display/>
+    </div>
+    <Player />
+    <audio ref={audioRef} src={track.file} preload='auto'/>
+   </div>
+  )
+}
+
+export default App
